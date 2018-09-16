0cc3701657733e6435b3e6010ea9db4297b4317b
thelaceyedit <thelaceyedit@gmail.com>
2018-09-16 19:34:34 -0400

    routes

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-var express =  require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-
-var http = require("http");
-
-var PORT = 3306;
-
-var server = http.createServer(function(req, res) {
-    res.end("Welcome to my server" + req.url);
-});
+var app = express();
+var PORT = 3000;
 
 
-server.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
-server.use(server.static(__dirname + '/app/public'));
+app.use(express.static(__dirname + '/app/public'));
 
 
-var htmlRoutes = require('./app/routing/htmlRoutes')(app, path);
-var apiRoutes = require('./app/routing/apiRoutes')(app, path);
+
+var htmlRoutes = require('./app/routes/htmlRoutes')(app, path);
+var apiRoutes = require('./app/routes/apiRoutes')(app, path);
 
 
-//start listening
-server.listen(PORT, function() {
-    console.log("Server listening on PORT " + PORT);
-});
+//start our server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+
