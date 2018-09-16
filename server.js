accec49726823d37b45741399a575d430ecf16c6
thelaceyedit <thelaceyedit@gmail.com>
2018-09-16 19:20:00 -0400

    adding routes

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 
-var app = express();
+var server = express();
 
 var PORT = process.env.PORT || 8081;
 
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
-
-
-app.use(express.static(__dirname + '/app/public'));
+server.use(bodyParser.json());
 
 
-var htmlRoutes = require('./app/routes/htmlRoutes')(app, path);
-var apiRoutes = require('./app/routes/apiRoutes')(app, path);
+server.use(express.static(__dirname + '/app/public'));
+
+
+var htmlRoutes = require('./app/routing/htmlRoutes')(app, path);
+var apiRoutes = require('./app/routing/apiRoutes')(app, path);
 
 
 //start listening
-app.listen(PORT, function() {
-    console.log("Application listening on PORT " + PORT);
+server.listen(PORT, function() {
+    console.log("Server listening on PORT " + PORT);
 });
