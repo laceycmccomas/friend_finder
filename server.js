42c53a2bff6bcfc07f77ec345e9d3810b9b76a7a
thelaceyedit <thelaceyedit@gmail.com>
2018-09-16 20:43:28 -0400

    routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use(express.static(__dirname + '/app/public'));
+// app.use(express.static(__dirname + '/app/public'));
 
 
 require("./routing/apiRoutes")(app);
@@ -21,5 +21,3 @@ require("./routing/htmlRoutes")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  
-
